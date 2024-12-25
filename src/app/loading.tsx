2a697332649d79cfe9f6be39e640f124d47280a9
Gourav soni <gouravsoni8476@gmail.com>
2024-12-25 14:51:39 +0530

    Enhance loading component with centered logo and animation for better user experience

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 const loading = () => {
-	return <div>loading</div>;
+	return (
+		<div className="min-w-full absolute min-h-screen flex justify-center items-center bg-[]">
+			<Image
+				src="/twtlogo.svg"
+				alt="loading"
+				width={80}
+				height={80}
+				className="animate-bounce"
+			/>
+		</div>
+	);
 };
 
 export default loading;
